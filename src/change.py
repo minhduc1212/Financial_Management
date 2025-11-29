ed77749ac45fa7128fb96f54133a5de2351e0e63
minhduc1212 <minhduc6e12@gmail.com>
2025-11-29 15:29:42 +0700

    update more func

---
+++ src/change.py
@@ -0,0 +1,39 @@
+import json
+
+#save the financial data to a JSON file
+with open('data/financial_data.json', 'r') as f:
+    data = json.load(f)
+    
+outcome = "spend bank entertainment 50000"
+action, payment_method, category, amount = outcome.split()
+amount = int(amount)
+
+#spend
+if action == "spend":
+    for item in data["spend"]:
+        if payment_method in item:
+            for cat in item[payment_method]:
+                if category in cat:
+                    cat[category] += amount
+                    if payment_method == "bank":
+                        data["balance"][0]["balance_bank"] -= amount
+                    elif payment_method == "cash":
+                        data["balance"][1]["balance_cash"] -= amount
+                    break
+            break
+
+#income
+elif action == "income":
+    for item in data["income"]:
+        if payment_method in item:
+            item[payment_method].append({category: amount})
+            
+            if payment_method == "bank":
+                data["balance"][0]["balance_bank"] += amount
+            elif payment_method == "cash": 
+                data["balance"][1]["balance_cash"] += amount
+            break
+        
+#save the updated financial data back to the JSON file
+with open('data/financial_data.json', 'w') as f:
+    json.dump(data, f, indent=4)
--- src/init.py
+++ src/init.py
@@ -1,3 +1,8 @@
 from create import create
 
-create(10000000, 10000000)
+print("Pls enter your bank and cash balance to initialize your financial data.")
+bank_balance = int(input("Enter your bank balance: "))
+cash_balance = int(input("Enter your cash balance: "))
+
+create(bank_balance, cash_balance)
+
