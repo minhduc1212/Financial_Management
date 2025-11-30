c3facafe3bdf2da03bc8048f1da66c6f8915fdcb
Đức Nguyễn Minh <minhduc6e12@gmail.com>
2025-11-30 08:56:57 +0700

    update stat.py

--- src/stat.py
+++ src/stat.py
@@ -1,11 +1,29 @@
 import matplotlib.pyplot as plt
+import json
 
-x = [1, 2, 3, 4]
-y = [10, 20, 25, 30]
+with open('data/financial_data.json', 'r') as file:
+    json_data = json.load(file)
+    spend = json_data['spend']
+    
+sum_spend_bank = 0
+for item in spend[0]["bank"]:
+    for category, amount in item.items():
+        sum_spend_bank += amount
 
-plt.plot(x, y)
-plt.title("Line Graph")
-plt.xlabel("X Axis")
-plt.ylabel("Y Axis")
+sum_spend_cash = 0
+for item in spend[1]["cash"]:
+    for category, amount in item.items():
+        sum_spend_cash += amount
+
+print(f"Total spend from bank: {sum_spend_bank}")
+print(f"Total spend from cash: {sum_spend_cash}")
+
+fig, ax = plt.subplots()
+ax.bar(['Bank', 'Cash'], [sum_spend_bank, sum_spend_cash])
+ax.set_ylabel('Total Spend')
+ax.set_title('Total Spend from Bank vs Cash')
 plt.show()
 
+
+
+    
