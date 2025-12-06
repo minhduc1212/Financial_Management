dc02d09af8f0f3a20e79b503b6e4ed05743993f8
Đức Nguyễn Minh <minhduc6e12@gmail.com>
2025-12-06 09:28:58 +0700

    update create.py: add period

--- src/create.py
+++ src/create.py
@@ -1,32 +1,55 @@
 import json
 import os
+import datetime
+
 def create(username,balance_bank, balance_cash):
+    
+    x = datetime.datetime.now()
+    period = x.strftime("%Y-%m")
 
     #create a financial data structure
     financial_data = {
         "balance": [
-            {"balance_bank": balance_bank},
-            {"balance_cash": balance_cash}
-        ],
-        "spend": [
-            {   
-            "bank": [
-                {"entertainment": 0},
-                {"food": 0},
-                {"other": 0}    
-                ]
+            {
+            "balance_bank": balance_bank
             },
             {
-            "cash": [
-                {"entertainment": 0},
-                {"food": 0},
-                {"other": 0}
-                ]
+            "balance_cash": balance_cash
             }
         ],
-        "income": [
-            {"bank": []},
-            {"cash": []}
+        "financial records":[
+            {
+                "period": period,
+                "expenses": 
+                { 
+                    "bank":
+                    {
+                        "entertainment": 0.0,
+                        "food": 0.0,
+                        "other": 0.0
+                    },
+                    "cash":
+                    {
+                        "entertainment": 0.0,
+                        "food": 0.0,
+                        "other": 0.0
+                    }
+                },
+                "income": 
+                {
+                    "bank": 
+                    {
+                        "salary": 0,
+                        "other": 0.0
+
+                    },
+                    "cash": 
+                    {
+                        "salary": 0,
+                        "other": 0.0
+                    }
+                }
+            }
         ]
     }
 
--- src/init.py
+++
@@ -1,8 +0,0 @@
-from create import create
-
-print("Pls enter your bank and cash balance to initialize your financial data.")
-bank_balance = int(input("Enter your bank balance: "))
-cash_balance = int(input("Enter your cash balance: "))
-
-create(bank_balance, cash_balance)
-
